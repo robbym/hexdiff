be331f335d0a7ce4e6689c134995f8da91088454
Robby Madruga <robbymadruga@gmail.com>
2019-12-08 16:10:08 -0700

    Fixed subsequent empty block not coalescing

--- src/diff.rs
+++ src/diff.rs
@@ -99,40 +99,43 @@ impl Iterator for IHex16DiffEngine {
     fn next(&mut self) -> Option<Self::Item> {
         let (address, value_1, value_2) = self.compare()?;
 
+        let mut next_address = address;
+        let mut next_value_1 = value_1;
+        let mut next_value_2 = value_2;
+
         if address > self.address {
-            let output = IHex16Diff::range(self.address, address - 4, 0xFFFFFF, 0xFFFFFF);
-            self.address = address;
-            return Some(output);
-        } else {
-            let mut next_address = address;
-            let mut next_value_1 = value_1;
-            let mut next_value_2 = value_2;
+            next_value_1 = 0xFFFFFF;
+            next_value_2 = 0xFFFFFF;
+        }
 
-            while next_value_1 == value_1 && next_value_2 == value_2 {
-                self.advance();
+        while next_value_1 == value_1 && next_value_2 == value_2 {
+            self.advance();
 
-                if let Some((na, nv1, nv2)) = self.compare() {
-                    if (na - 4) != next_address {
+            if let Some((na, nv1, nv2)) = self.compare() {
+                if (na - 4) != next_address {
+                    if next_value_1 == 0xFFFFFF &&  next_value_2 == 0xFFFFFF {
+                        next_address = na - 4;
+                    } else {
                         break;
                     }
-
-                    next_address += 4;
-                    next_value_1 = nv1;
-                    next_value_2 = nv2;
-                } else {
-                    break;
                 }
-            }
 
-            if address == next_address - 4 {
-                let output = IHex16Diff::single(address, value_1, value_2);
-                self.address += 4;
-                Some(output)
+                next_address += 4;
+                next_value_1 = nv1;
+                next_value_2 = nv2;
             } else {
-                let output = IHex16Diff::range(address, next_address - 4, value_1, value_2);
-                self.address = next_address;
-                Some(output)
+                break;
             }
+        }
+
+        if address == next_address - 4 {
+            let output = IHex16Diff::single(address, value_1, value_2);
+            self.address += 4;
+            Some(output)
+        } else {
+            let output = IHex16Diff::range(address, next_address - 4, value_1, value_2);
+            self.address = next_address;
+            Some(output)
         }
     }
 }
